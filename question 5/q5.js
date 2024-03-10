005edee36e7a65dcd12f190dd307f49336521368
TALHAdevelops <160895671+TALHAdevelops@users.noreply.github.com>
2024-03-10 21:45:16 +0500

    Add files via upload

---
+++ question 5/q5.js
@@ -0,0 +1,4 @@
+"use strict";
+let famous_person = "Muhammad Ali";
+let message = "I hated every minute of training, but I said, 'Don't quit. Suffer now and live the rest of your life as a champion.'";
+console.log(`${famous_person} once said: ${message}`);
---
+++ question 5/q5.ts
@@ -0,0 +1,4 @@
+let famous_person="Muhammad Ali";
+let message="I hated every minute of training, but I said, 'Don't quit. Suffer now and live the rest of your life as a champion.'";
+
+console.log(`${famous_person} once said: ${message}`);
