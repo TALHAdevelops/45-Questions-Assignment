e16ad368aba989e16ea97a93300d1fc89c206c64
TALHAdevelops <160895671+TALHAdevelops@users.noreply.github.com>
2024-03-10 21:44:19 +0500

    Add files via upload

---
+++ question 2/q2.js
@@ -0,0 +1,3 @@
+"use strict";
+let personName = "Talha";
+console.log(`Hello ${personName},would you like to learn Python today?`);
---
+++ question 2/q2.ts
@@ -0,0 +1,3 @@
+let personName="Talha";
+
+console.log(`Hello ${personName},would you like to learn Python today?`);
