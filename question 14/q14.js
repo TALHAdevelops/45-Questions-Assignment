7581fff16f52814128593f2338fead020c460b8d
TALHAdevelops <160895671+TALHAdevelops@users.noreply.github.com>
2024-03-10 21:48:37 +0500

    Add files via upload

---
+++ question 14/q14.js
@@ -0,0 +1,4 @@
+"use strict";
+let GuestsName = ["Talha!", "Hamza!", "Hassaan!"];
+for (let index = 0; index < GuestsName.length; index++)
+    console.log("Please come to dinner at my home. MR." + GuestsName[index]);
---
+++ question 14/q14.ts
@@ -0,0 +1,3 @@
+let GuestsName = ["Talha!", "Hamza!", "Hassaan!"];
+for (let index = 0; index < GuestsName.length; index++)
+ console.log("Please come to dinner at my home. MR." + GuestsName[index]);
