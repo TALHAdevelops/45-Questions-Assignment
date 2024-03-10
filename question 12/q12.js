f14004be13ace353d3a7dee920dfdfb876f91b88
TALHAdevelops <160895671+TALHAdevelops@users.noreply.github.com>
2024-03-10 21:47:48 +0500

    Add files via upload

---
+++ question 12/q12.js
@@ -0,0 +1,4 @@
+"use strict";
+let MyFriends = ["Hamza! ", "Hassaan! ", "Talha! "];
+for (let index = 0; index < MyFriends.length; index++)
+    console.log("this is my friend " + MyFriends[index]);
---
+++ question 12/q12.ts
@@ -0,0 +1,5 @@
+let MyFriends=["Hamza! ","Hassaan! ","Talha! "];
+for (let index = 0; index < MyFriends.length; index++) 
+    
+    console.log("this is my friend "+MyFriends[index]);
+    
