90176f199f7ae10aa9f070a758be1d0e146d2375
TALHAdevelops <160895671+TALHAdevelops@users.noreply.github.com>
2024-03-10 21:48:19 +0500

    Add files via upload

---
+++ question 13/q13.js
@@ -0,0 +1,4 @@
+"use strict";
+let myTransportation = ["Supra", "Ninja H2R", "Bugatti", "Porche"];
+for (let i = 0; i < myTransportation.length; i++)
+    console.log("I would like to own" + " " + myTransportation[i]);
---
+++ question 13/q13.ts
@@ -0,0 +1,5 @@
+let myTransportation=["Supra","Ninja H2R","Bugatti","Porche"];
+for (let i= 0; i< myTransportation.length; i++) 
+
+console.log("I would like to own"+" "+myTransportation[i])
+    
