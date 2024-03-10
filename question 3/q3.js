a285f862855183f66c47ae134490d89e173f1425
TALHAdevelops <160895671+TALHAdevelops@users.noreply.github.com>
2024-03-10 21:44:37 +0500

    Add files via upload

---
+++ question 3/q3.js
@@ -0,0 +1,5 @@
+"use strict";
+let myName = "babar";
+console.log(myName.toUpperCase());
+console.log(myName.toLowerCase());
+console.log(myName.replace(/\bw/g, c => c.toUpperCase()));
---
+++ question 3/q3.ts
@@ -0,0 +1,5 @@
+let myName="babar";
+
+console.log(myName.toUpperCase());
+console.log(myName.toLowerCase());
+console.log(myName.replace(/\bw/g,c => c.toUpperCase()));
