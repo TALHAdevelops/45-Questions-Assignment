5c01ed132e394604add3c5ed3285a961529611a8
TALHAdevelops <160895671+TALHAdevelops@users.noreply.github.com>
2024-03-10 21:45:43 +0500

    Add files via upload

---
+++ question 7/q7.js
@@ -0,0 +1,9 @@
+"use strict";
+let additionForEight = 5 + 3;
+console.log(additionForEight);
+let subtractionForEight = 24 - 16;
+console.log(subtractionForEight);
+let multiplicationForEight = 4 * 2;
+console.log(multiplicationForEight);
+let divisionForEight = 16 / 2;
+console.log(divisionForEight);
---
+++ question 7/q7.ts
@@ -0,0 +1,11 @@
+let additionForEight= 5+3 ;
+console.log(additionForEight);
+
+let subtractionForEight= 24-16 ;
+console.log(subtractionForEight);
+
+let multiplicationForEight= 4*2 ;
+console.log(multiplicationForEight);
+
+let divisionForEight= 16/2 ;
+console.log(divisionForEight);
