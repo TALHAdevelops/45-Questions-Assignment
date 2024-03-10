4b4122d007032dbac65888afd09c40141d046b4d
TALHAdevelops <160895671+TALHAdevelops@users.noreply.github.com>
2024-03-10 21:46:53 +0500

    Add files via upload

---
+++ question 10/q10.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("Hello World Program");
+//this is my first simple program 
+console.log(34 + 32);
+//these are operators which are called arthmetic operators
---
+++ question 10/q10.ts
@@ -0,0 +1,4 @@
+console.log("Hello World Program");
+//this is my first simple program 
+console.log(34+32);
+//these are operators which are called arthmetic operators
